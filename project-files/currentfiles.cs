8719aa01c94486ec37e6cf72d158ff47534dffcb
rezaice07 <rezaice07@gmail.com>
2019-12-02 21:24:36 +0600

    added project files

---
+++ project-files/currentfiles.cs
@@ -0,0 +1,36 @@
+--select directory
+cd /D
+
+--select folder
+cd your_folder_name/
+
+--configure git
+
+git config --global user.name rezaice07
+
+press ennter key
+
+git config --global user.email rezaice07@gmail.com
+
+press ennter key
+
+ennter your password....
+
+--clone git repository
+
+git clone https://github.com/rezaice07/DLMS.git
+
+
+--add file in git repository
+
+git add file_name
+
+--check status
+git status
+
+--commit file
+git commit -m 'Added Demo.txt' Demo.txt
+
+--transfer file to git repository
+git push -u origin master
+
